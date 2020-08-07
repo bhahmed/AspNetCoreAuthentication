63b2ad22433f98816b548e2e8746a40a941e9d5a
Ahmed Ben Hmida <ahmed.benhmida@ensi-uma.tn>
2020-08-07 11:45:28 +0100

    Basic Authentication

--- Basics/Startup.cs
+++ Basics/Startup.cs
@@ -16,6 +16,14 @@ namespace Basics
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication("CookieAuth")
+                .AddCookie("CookieAuth", config =>
+                {
+                    config.Cookie.Name = "Grandmas.Cookie";
+                    config.LoginPath = "/Home/Authenticate";
+                });
+
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +36,15 @@ namespace Basics
 
             app.UseRouting();
 
+            // who you are?
+            app.UseAuthentication();
+            
+            // are you allowed?
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
